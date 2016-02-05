a47a032a5d9d359e0d81e04cda7db06a7010c7d5
pboyle17 <pboyle17@gmail.com>
2016-02-04 18:42:40 -0600

    add command to rename all directories in homefolder to lowercase

--- setup.sh
+++ setup.sh
@@ -29,3 +29,5 @@ sudo add-apt-repository ppa:webupd8team/atom
 sudo apt-get update
 sudo apt-get install atom
 echo atom installed!!
+echo renaming lowercase directory names
+mv Desktop/ desktop && mv Downloads/ downloads && mv Music/ music && mv Pictures/ pictures && mv Public/ public && mv Templates/ templates && mv Videos/ videos
